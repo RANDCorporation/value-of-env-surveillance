1d8900bacfcfe1bc54cad52253901503cd39077c
Pedro Nascimento de Lima <plima@rand.org>
2023-04-07 14:04:50 -0400

    updating metapopulation model with no deaths

--- R/scripts/00_metapopulation_no_deaths.R
+++ R/scripts/00_metapopulation_no_deaths.R
@@ -1,106 +1,19 @@
 
 
-# Metapopulation model
-# Originally from:
-# http://epirecip.es/epicookbook/chapters/lloydjansen2004/r_odin
+# Metapopulation model implemented using the odin package.
+# See https://mrc-ide.github.io/odin/
+# for documentation on using odin
+# And
+# for examples of models implemented using odin
+# This model is inspired in http://epirecip.es/epicookbook/chapters/lloydjansen2004/r_odin
+# Which provides an implementation of a metapopulation model
+# with coupling and/or migration between patches
+# https://www.sciencedirect.com/science/article/abs/pii/S0025556403001809?via%3Dihub
 
 library(odin)
 
 
 # Helper functions:
-
-# beta matrix: random initialisation -----------------------------------------------------
-beta.mat <- function (nr_patches) {
-  ## beta: positive, non-symmetric matrix of effective contact rates
-  ## values are higher on the diagonal (transmission is higher *within*-patch),
-  ## values decrease gradually further away from the diagonal (*between*-patch transmission)
-  beta <- matrix(0, nrow=nr_patches, ncol=nr_patches)
-  for (i in 0:(nr_patches-1)) {
-    ## superdiagonal: scale vector s.t. numbers decrease further away from diagonal
-    rand.vec <- rnorm(nr_patches-i, 10 * (nr_patches-i)/nr_patches, 2)
-    beta[row(beta) == col(beta) - i] <- rand.vec
-    ## subdiagonal
-    rand.vec <- rnorm(nr_patches-i, 10 * (nr_patches-i)/nr_patches, 2)
-    beta[row(beta) == col(beta) - i] <- rand.vec
-  }
-  return(beta)
-}
-
-# mobility matrix: random initialisation -----------------------------------------------------
-mob.mat <- function (nr_patches) {
-  ## mobility matrix (origin-destination matrix of proportion of population that travels) #TODO trip counts or relative?
-  C <- matrix(0, nrow=nr_patches, ncol=nr_patches)
-  if (nr_patches > 1) {
-    diag(C) <- -sample(1:25, nr_patches, replace=TRUE)
-    for (i in 1:nr_patches) {
-      C[i, which(C[i, ] == 0)] <- rmultinom(n = 1, size = -C[i,i], prob = rep(1, nr_patches-1))
-    }
-    C <- C/100
-  }
-  return(C)
-}
-
-
-# plotting --------------------------------------------------------------------#
-plot.pretty <- function(out, nr_patches, what) {
-
-  # plot total densities by disease status --------------------------------------#
-  if (what == "total") {
-    ## compute total densities by disease status
-    S_tot <- rowSums(out$S) / nr_patches
-    E_tot <- rowSums(out$E) / nr_patches
-    P_tot <- rowSums(out$P) / nr_patches
-    I_tot <- rowSums(out$I) / nr_patches
-    R_tot <- rowSums(out$R) / nr_patches
-
-    ## plot total densities by disease status
-    par(mfrow=c(1, 1), las=1, omi=c(1,0,0,0), xpd=NA)
-    plot( t, S_tot, col="green",
-          type="l", xlab="Days", ylab="Densities",
-          main="Total densities by disease status")
-    lines(t, E_tot, col="yellow")
-    lines(t, P_tot, col="orange")
-    lines(t, I_tot, col="red")
-    lines(t, R_tot, col="blue")
-    legend(-8.5, -0.3, title="Disease statuses", horiz=TRUE,
-           legend=c("Susceptible", "Exposed", "Pre-Symptomatic", "Infectious", "Recovered"),
-           col=c("green", "yellow", "orange", "red", "blue"), lty=1)
-  }
-
-  # plot densities of some patches by disease status ----------------------------#
-  if (what == "panels") {
-    ## define the plot panels
-    if (nr_patches >= 6) {
-      mfrow=c(2, 3)
-      panels <- as.integer( seq(1, nr_patches, length.out=6))
-    } else if (nr_patches >= 4) {
-      mfrow=c(2, 2)
-      panels <- as.integer( seq(1, nr_patches, length.out=4))
-    } else {
-      mfrow=c(1, nr_patches)
-      panels <- 1:nr_patches
-    }
-    par(mfrow=mfrow, las=1, omi=c(1,0,0.3,0), xpd=NA)
-
-    ## plot the disease statuses of some patches
-    ymax <- max(out$N[, panels])
-    for (i in panels) {
-      plot (t, out$S[, i], col="green", ylim=c(0, ymax),
-            type="l", xlab="Days", ylab="Densities",
-            main=paste("Patch ", i))
-      lines(t, out$E[, i], col="orange")
-      lines(t, out$I[, i], col="red")
-      lines(t, out$R[, i], col="blue")
-    }
-    title("Densities by disease status", outer=TRUE)
-    legend(-130, -1.2, title="Disease statuses", horiz=TRUE,
-           legend=c("Susceptible", "Exposed", "Infectious", "Recovered"),
-           col=c("green", "orange", "red", "blue"), lty=1)
-  }
-
-}
-
-
 
 SEIR_cont <- odin::odin({
   nr_patches <- user(2)
@@ -247,6 +160,70 @@ out <- mod$transform_variables(out)
 # error check -----------------------------------------------------------------#
 if ( ! all( abs(rowSums(out$N) - nr_patches) < 1E-10 ) )
   warning("Something went wrong, density is increasing/decreasing!\n")
+
+
+
+
+# plotting --------------------------------------------------------------------#
+plot.pretty <- function(out, nr_patches, what) {
+
+  # plot total densities by disease status --------------------------------------#
+  if (what == "total") {
+    ## compute total densities by disease status
+    S_tot <- rowSums(out$S) / nr_patches
+    E_tot <- rowSums(out$E) / nr_patches
+    P_tot <- rowSums(out$P) / nr_patches
+    I_tot <- rowSums(out$I) / nr_patches
+    R_tot <- rowSums(out$R) / nr_patches
+
+    ## plot total densities by disease status
+    par(mfrow=c(1, 1), las=1, omi=c(1,0,0,0), xpd=NA)
+    plot( t, S_tot, col="green",
+          type="l", xlab="Days", ylab="Densities",
+          main="Total densities by disease status")
+    lines(t, E_tot, col="yellow")
+    lines(t, P_tot, col="orange")
+    lines(t, I_tot, col="red")
+    lines(t, R_tot, col="blue")
+    legend(-8.5, -0.3, title="Disease statuses", horiz=TRUE,
+           legend=c("Susceptible", "Exposed", "Pre-Symptomatic", "Infectious", "Recovered"),
+           col=c("green", "yellow", "orange", "red", "blue"), lty=1)
+  }
+
+  # plot densities of some patches by disease status ----------------------------#
+  if (what == "panels") {
+    ## define the plot panels
+    if (nr_patches >= 6) {
+      mfrow=c(2, 3)
+      panels <- as.integer( seq(1, nr_patches, length.out=6))
+    } else if (nr_patches >= 4) {
+      mfrow=c(2, 2)
+      panels <- as.integer( seq(1, nr_patches, length.out=4))
+    } else {
+      mfrow=c(1, nr_patches)
+      panels <- 1:nr_patches
+    }
+    par(mfrow=mfrow, las=1, omi=c(1,0,0.3,0), xpd=NA)
+
+    ## plot the disease statuses of some patches
+    ymax <- max(out$N[, panels])
+    for (i in panels) {
+      plot (t, out$S[, i], col="green", ylim=c(0, ymax),
+            type="l", xlab="Days", ylab="Densities",
+            main=paste("Patch ", i))
+      lines(t, out$E[, i], col="orange")
+      lines(t, out$I[, i], col="red")
+      lines(t, out$R[, i], col="blue")
+    }
+    title("Densities by disease status", outer=TRUE)
+    legend(-130, -1.2, title="Disease statuses", horiz=TRUE,
+           legend=c("Susceptible", "Exposed", "Infectious", "Recovered"),
+           col=c("green", "orange", "red", "blue"), lty=1)
+  }
+
+}
+
+
 
 # plot total densities by disease status --------------------------------------#
 plot.pretty(out, nr_patches, "total")
