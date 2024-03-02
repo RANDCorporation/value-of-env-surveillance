5261335a93355797e1c0c260dd4c183db23c6f0d
Pedro Nascimento de Lima <plima@rand.org>
2024-03-02 08:36:35 -0500

    adding remotes as a dependency

--- R/scripts/dependencies.R
+++ R/scripts/dependencies.R
@@ -7,7 +7,7 @@ dependencies <- renv::dependencies(path = "./R")
 
 packages <- unique(dependencies$Package)
 
-cran_packages <- c("Hmisc", "R6", "tidyr", "odin", "dplyr", "ggplot2", "dde",
+cran_packages <- c("remotes", "Hmisc", "R6", "tidyr", "odin", "dplyr", "ggplot2", "dde",
                   "gt", "lemon", "lubridate", "parallel", "purrr", "readxl", "scales", "writexl", "yaml")
 
 rand_packages_github <- c("R6Sim", "randplot")
